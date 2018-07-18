6da96516db1689d7370e89c6d3a18ba646258b0f
Wendy Chang <wwc30@georgetown.edu>
2018-07-18 15:38:06 -0400

    added test specs

--- sagas.js
+++ sagas.js
@@ -1,12 +1,14 @@
 import { delay } from 'redux-saga'
-import { put, takeEvery, all } from 'redux-saga/effects'
+import { put, takeEvery, all, call } from 'redux-saga/effects'
 
 export function* helloSaga() {
   console.log('Hello Sagas!')
 }
 
 export function* incrementAsync() {
-  yield delay(1000)
+  // neither put nor call performs any dispatch or asynchronous call by themselves, they simply return plain JavaScript objects
+
+  yield call(delay, 1000) // returns an effect which instructs the middleware to call a given function with the given arguments
   yield put({ type: 'INCREMENT' })
 }
 
